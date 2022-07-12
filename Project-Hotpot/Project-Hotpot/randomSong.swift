ee951734547d8cd61a1571fd8842237564955d22
Harini Sundaram <harinisundaram@fb.com>
2022-07-12 10:38:28 -0700

    created play/pause buttons, with functionality

---
+++ Project-Hotpot/Project-Hotpot/randomSong.swift
@@ -0,0 +1,13 @@
+//
+//  randomSong.swift
+//  Project-Hotpot
+//
+//  Created by Harini Sundaram on 7/11/22.
+//
+
+import Foundation
+
+func getRandomSong() -> String{
+    return "spotify:track:20I6sIOMTCkB6w7ryavxtO"
+}
+
