a2bd8da7d4ae41c762e0dd7d78282ac36c8a0047
Harini Sundaram <harinisundaram@fb.com>
2022-07-14 10:57:37 -0700

    created saveSong method that converts a Spotify Song object to a PFObject

--- Project-Hotpot/Project-Hotpot/PFSong.swift
+++ Project-Hotpot/Project-Hotpot/PFSong.swift
@@ -12,14 +12,29 @@ import Parse
 class PFSong: PFObject, PFSubclassing {
     @NSManaged var name : String
     @NSManaged var URI: String
-    @NSManaged var duration: Int
+    @NSManaged var duration: UInt
     @NSManaged var artist: String
     @NSManaged var album: String
     
     static func parseClassName() -> String {
         return "Song"
     }
-
+    
+    class func saveSong(song:SPTAppRemoteTrack) {
+       // use subclass approach
+       let newSong = PFSong()
+       
+        // Add relevant fields to the object
+        newSong.name = song.name
+        newSong.URI = song.uri
+        newSong.duration = song.duration
+        newSong.artist = song.artist.name
+        newSong.album = song.album.name
+        
+       // Save object (following function will save the object in Parse asynchronously)
+        newSong.saveInBackground()
+    
+    }
 }
 
 
