3bdcb5cc73bc561f36d5be8be7b30123816503b1
Harini Sundaram <harinisundaram@fb.com>
2022-07-14 15:00:55 -0700

    created test function to make sure I can query back the PFSongs I added to the database

--- Project-Hotpot/Project-Hotpot/Algorithm/randomSong.swift
+++ Project-Hotpot/Project-Hotpot/Algorithm/randomSong.swift
@@ -6,8 +6,30 @@
 //
 
 import Foundation
+import Parse
+
+var previous_array: [PFSong] = []
 
 func getRandomSong() -> String{
     return "spotify:track:20I6sIOMTCkB6w7ryavxtO"
+}
+
+//this is a testing function, to make sure that I can query like I want to going forward
+
+func getPreviousSongs() {
+    
+    let query = PFQuery(className:PFSong.parseClassName())
+    query.findObjectsInBackground { (objects: [PFObject]?, error: Error?) -> Void in
+        if error != nil {
+            print(error?.localizedDescription)
+        } else if let objects = objects as? [PFSong]{
+            //I'm making sure I am able to access the PFSong objects
+            for obj in objects{
+                NSLog(obj.name)
+            }
+
+        }
+ 
+    }
 }
 
